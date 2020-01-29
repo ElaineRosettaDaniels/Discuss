e6a103551942c4c8ff0b6faa88ec14a7bb86d129
ElaineRosettaDaniels <elainedaniels457@gmail.com>
2020-01-29 16:55:07 -0500

    lesson progress, now into OAuth

--- config/config.exs
+++ config/config.exs
@@ -25,3 +25,12 @@ config :logger, :console,
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: {Ueberauth.Strategy.Github, []}
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id: System.get_env("92582482518102f8502d"),
+  client_secret: System.get_env("1cc49443a6d65852fb7a7dcead7d9c05c3ebf3ab")
