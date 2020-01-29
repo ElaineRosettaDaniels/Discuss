2bf4bea718a0acfdf756e73134478f13651c46de
ElaineRosettaDaniels <elainedaniels457@gmail.com>
2020-01-29 09:02:28 -0500

    lesson progress

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -3,22 +3,34 @@ defmodule Discuss.TopicController do
 
     alias Discuss.Topic
 
+    def index(conn, _params) do
+        topics = Repo.all(Topic)
+        render conn, "index.html", topics: topics 
+    end
+
     def new(conn, _params) do
         changeset = Topic.changeset(%Topic{}, %{})
-        #This line tells us when we're making a new doc, we want to provide the new page w/ our changeset
         render conn, "new.html", changeset: changeset
     end
-    # topic is our params
+
     def create(conn, %{"topic" => topic}) do
-        # %Topic{} is an empty struct, we pass in the params: topic
+
         changeset = Topic.changeset(%Topic{}, topic)
 
-        # We use the Repo module to pass in our changeset
         case Repo.insert(changeset) do
-            {:ok, post} -> IO.inspect(post)
+            {:ok, post} -> 
+                conn
+                |> put_flash(:info, "Topic Created") # put_flash displays a message that will be shown to the user once, then go away after refresh
+                |> redirect(to: topic_path(conn, :index))
             {:error, changeset} -> 
-                # we're gonna render the page again with the changeset we had before if we get an error
                 render conn, "new.html", changeset: changeset
         end
+    end
+
+    def edit(conn, %{"id" => topic_id}) do
+        topic = Repo.get(Topic, topic_id)
+        changeset = Topic.changeset(topic)
+
+        render conn, "edit.html", changeset: changeset, topic: topic # we need this 'topic: topic' at the end to get the right ID we're working with
     end
 end
