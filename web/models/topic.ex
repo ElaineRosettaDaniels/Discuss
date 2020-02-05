624ffae748ada75e9057e95ebe34e34629d780e2
ElaineRosettaDaniels <elainedaniels457@gmail.com>
2020-02-05 16:55:30 -0500

    daily update

--- web/models/topic.ex
+++ web/models/topic.ex
@@ -1,6 +1,6 @@
 defmodule Discuss.Topic do
     use Discuss.Web, :model
-
+    # update
     schema "topics" do
         field :title, :string
         belongs_to :user, Discuss.User # Says this instance belongs to a thing--a *user*, and an instance of that user is from Discuss.User
