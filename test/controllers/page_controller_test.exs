2d7b9acfca73fe33c61c094e1aaea8cf24192e39
ElaineRosettaDaniels <elainedaniels457@gmail.com>
2020-02-11 12:25:51 -0500

    updated

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -1,6 +1,6 @@
 defmodule Discuss.PageControllerTest do
   use Discuss.ConnCase
-
+  #update
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
     assert html_response(conn, 200) =~ "Welcome to Phoenix!"
